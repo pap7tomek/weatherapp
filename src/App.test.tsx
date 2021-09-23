08e6c58511691f7c9a7973c0d6cdf1f21a4629cc
Tomek <pap7tomek@gmail.com>
2021-09-23 20:32:19 +0200

    tests and fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import { Form } from './components/form/Form'
+
+
+// test('loads and displays greeting', async () => {
+//   render(<App/>)
+
+//   // fireEvent.click(screen.getByText('Load Greeting'))
+
+//   // await waitFor(() => screen.getByRole('heading'))
+
+//   expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+//   expect(screen.getByRole('button')).toBeDisabled()
+// })
+test('button exists and has good name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Dodaj/i);
+  expect(element).toBeInTheDocument();
 });
+
+test('has input', () => {
+  render(<App />);
+  const element = screen.getByRole('textbox');
+  expect(element).toBeInTheDocument();
+})
